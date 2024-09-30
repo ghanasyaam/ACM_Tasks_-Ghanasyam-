ff34734f2ee1d29ee5592fe42535464375d4a443
ghanasyaam <ghanasyaaam@gmail.com>
2024-09-30 17:31:28 +0530

    Task 3 Completed

---
+++ Task 03 Smart Contract/MyToke.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleToken {
+    string public name = "SimpleToken";
+    string public symbol = "STK";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Mint(address indexed to, uint256 value);
+
+    function mint(address _to, uint256 _value) public {
+        totalSupply += _value;
+        balanceOf[_to] += _value;
+        emit Mint(_to, _value);
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function checkBalance(address _owner) public view returns (uint256 balance) {
+        return balanceOf[_owner];
+    }
+}
